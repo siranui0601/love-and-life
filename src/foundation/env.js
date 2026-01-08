c2b206d87891dbfeb847d893feeeeafa4d1a4087
siranui0601 <41378727+siranui0601@users.noreply.github.com>
2026-01-09 07:31:51 +0900

    env.js を作成

---
+++ src/foundation/env.js
@@ -0,0 +1,11 @@
+import "dotenv/config";
+
+export const serviceAccount = process.env.GOOGLE_SERVICE_ACCOUNT_KEY
+  ? JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY)
+  : null;
+
+export const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
+export const SHEET_NAME = process.env.SHEET_NAME || "Users";
+
+export const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+export const PORT = process.env.PORT || 3000;
