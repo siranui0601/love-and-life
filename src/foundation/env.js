b51000d758f986238916d49564227a93ee4daf70
siranui0601 <41378727+siranui0601@users.noreply.github.com>
2026-01-13 21:39:51 +0900

    env.js を更新

--- src/foundation/env.js
+++ src/foundation/env.js
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config({ path: new URL("../../.env", import.meta.url).pathname });
 
 export const serviceAccount = process.env.GOOGLE_SERVICE_ACCOUNT_KEY
   ? JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY)
