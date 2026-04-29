8d8b4a029e863c56ccd83a76af7702162e7544b0
siranui0601 <41378727+siranui0601@users.noreply.github.com>
2026-04-30 07:54:38 +0900

    main.js を更新

--- public/origin-magic-circle/main.js
+++ public/origin-magic-circle/main.js
@@ -228,8 +228,8 @@ try {
   camera.lookAt(enemyPos.clone().add(new Vector3(0, 3.5, 0)));
 };
 
-  const pedestalA = makeSceneObject(pedestalGltf.scene, 0.01);
-const pedestalB = makeSceneObject(pedestalGltf.scene, 0.03);
+  const pedestalA = makeSceneObject(pedestalGltf.scene, 0.008);
+const pedestalB = makeSceneObject(pedestalGltf.scene, 0.008);
 pedestalA.position.set(-8, 0, 0);
 pedestalB.position.set(8, 0, 0);
 scene.add(pedestalA, pedestalB);
