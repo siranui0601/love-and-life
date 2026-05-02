9de561b9b65d2e22da7b943c5044724d1183f6a8
siranui0601 <41378727+siranui0601@users.noreply.github.com>
2026-05-03 00:49:34 +0900

    main.js を更新

--- public/origin-magic-circle/main.js
+++ public/origin-magic-circle/main.js
@@ -596,9 +596,9 @@ fighterB.position.set(16, fighterYOffset, 0);
   const root = gltf.scene.clone(true);
   root.visible = false;
   //変更11
-  if (assetName === "stylized_fire_tornado.glb") {
-    applyFireTornadoMaterialFix(root);
-  }
+  //if (assetName === "stylized_fire_tornado.glb") {
+    //applyFireTornadoMaterialFix(root);
+  //}
   root.position.set(0, 1.6, 0);
   root.scale.setScalar(1);
   scene.add(root);
