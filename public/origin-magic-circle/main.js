4501956a4ebbc88f841d27b7d8951a23e8149e02
siranui0601 <41378727+siranui0601@users.noreply.github.com>
2026-05-05 22:56:23 +0900

    main.js を更新

--- public/origin-magic-circle/main.js
+++ public/origin-magic-circle/main.js
@@ -196,7 +196,7 @@ const summonAssetSizePresets = {
 };
 
 //変更14
-const ENABLE_ORIGIN_DEBUG = false;
+const ENABLE_ORIGIN_DEBUG = true;
 
 function showDebug(text) {
   if (!ENABLE_ORIGIN_DEBUG) return;
