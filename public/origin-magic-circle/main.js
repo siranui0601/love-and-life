842a6c5d5f4831dd5e5482b216f4664bb4535aa3
siranui0601 <41378727+siranui0601@users.noreply.github.com>
2026-05-07 20:51:48 +0900

    main.js を更新

--- public/origin-magic-circle/main.js
+++ public/origin-magic-circle/main.js
@@ -4446,7 +4446,7 @@ title.innerHTML = `
   const canvas = document.createElement("canvas");
   canvas.className = "origin-result-bubble__canvas";
   canvas.width = 260;
-  canvas.height = 560;
+  canvas.height = 180;
 
   bubble.append(title, canvas);
 
