14be1258535839a45fce6402dd6e43923283922d
siranui0601 <41378727+siranui0601@users.noreply.github.com>
2026-09-01 21:54:02 +0900

    style(now-coding): load v4 typed renderer layer

--- public/now-coding/app.js
+++ public/now-coding/app.js
@@ -1,1 +1,6 @@
+const typedStyle = document.createElement("link");
+typedStyle.rel = "stylesheet";
+typedStyle.href = "./style-v4.css";
+document.head.appendChild(typedStyle);
+
 import "./app-v3.js";
