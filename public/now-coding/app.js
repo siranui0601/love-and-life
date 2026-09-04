c1ab44231e0c796e99f053e95352d76094d4fe69
siranui0601 <41378727+siranui0601@users.noreply.github.com>
2026-09-04 09:11:21 +0900

    fix(now-coding): install editor stability guard before app boot

--- public/now-coding/app.js
+++ public/now-coding/app.js
@@ -1,6 +1,7 @@
+import "./editor-scroll-stability.js";
+import "./app-v3.js";
+
 const typedStyle = document.createElement("link");
 typedStyle.rel = "stylesheet";
 typedStyle.href = "./style-v4.css";
 document.head.appendChild(typedStyle);
-
-import "./app-v3.js";
